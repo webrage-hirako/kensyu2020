8a98246a336095a400a55048408036aeab50f258
Kensuke HIRAKO <hirako-kensuke@webrage.jp>
2020-04-24 15:22:42 +0900

    first commit.

---
+++ gittest.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>gitlabテストページ</title>
+	</head>
+	<body>
+		welcome to hrdiv test directory.<br/>
+		横浜優勝！！<br/>
+    GITLab test.
+	</body>
+</html>
